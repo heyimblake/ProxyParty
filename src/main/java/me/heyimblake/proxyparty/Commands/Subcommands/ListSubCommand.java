641dcf2ba14f244e7f763793e9e8436d5b5dab8b
heyimblake <heyimblake@users.noreply.github.com>
2016-10-23 15:00:25 -0400

    Add list command logic

--- src/main/java/me/heyimblake/proxyparty/Commands/Subcommands/ListSubCommand.java
+++ src/main/java/me/heyimblake/proxyparty/Commands/Subcommands/ListSubCommand.java
@@ -3,7 +3,12 @@ package me.heyimblake.proxyparty.Commands.Subcommands;
 import me.heyimblake.proxyparty.Commands.AnnotatedPartySubCommand;
 import me.heyimblake.proxyparty.Commands.PartySubCommandExecutor;
 import me.heyimblake.proxyparty.Commands.PartySubCommandHandler;
+import me.heyimblake.proxyparty.PartyUtils.Party;
+import me.heyimblake.proxyparty.PartyUtils.PartyManager;
+import me.heyimblake.proxyparty.Utils.Constants;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 /**
  * Created by heyimblake on 10/21/2016.
@@ -25,7 +30,28 @@ public class ListSubCommand extends AnnotatedPartySubCommand {
 
     @Override
     public void runProxiedPlayer() {
+        ProxiedPlayer player = ((ProxiedPlayer) getHandler().getCommandSender());
+        Party party = PartyManager.getInstance().getPartyOf(player);
+        if (party == null) {
+            TextComponent msg = new TextComponent("You are not in a party.");
+            msg.setColor(ChatColor.RED);
+            player.sendMessage(Constants.TAG, msg);
+            return;
+        }
+        TextComponent line1 = new TextComponent("Party Leader: ");
+        line1.setColor(ChatColor.YELLOW);
+        line1.addExtra(new TextComponent(party.getLeader().getName()));
 
+        TextComponent line2 = new TextComponent("Participants: ");
+        line2.setColor(ChatColor.AQUA);
+
+        String allParticipants = "";
+        party.getParticipants().forEach(participants -> allParticipants.concat(participants.getName() + ", "));
+        TextComponent line3 = new TextComponent(allParticipants);
+
+        player.sendMessage(Constants.TAG, line1);
+        player.sendMessage(Constants.TAG, line2);
+        player.sendMessage(Constants.TAG, line3);
     }
 
     @Override
